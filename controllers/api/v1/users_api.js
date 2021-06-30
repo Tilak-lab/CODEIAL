436a0b9ca07c659871ca61ec1962edfd3e6ee588
Tilak-lab1 <85940835+Tilak-lab1@users.noreply.github.com>
2021-06-30 11:56:04 +0530

    Small minor change

--- controllers/api/v1/users_api.js
+++ controllers/api/v1/users_api.js
@@ -16,7 +16,7 @@ module.exports.createSession = async function(req, res){
         return res.json(200, {
             message: 'Sign in successful, here is your token, please keep it safe!',
             data:  {
-                token: jwt.sign(user.toJSON(), env.jwtkey_secret, {expiresIn:  '10000'}) 
+                token: jwt.sign({email:req.body.email}, env.jwtkey_secret, {expiresIn:  '10000'}) 
             }
         })
 
