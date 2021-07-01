b8851cddc58aad4013519fcdc0269757d53dd8ce
Tilak-lab1 <85940835+Tilak-lab1@users.noreply.github.com>
2021-07-01 20:12:08 +0530

    Changed the localhost of sockets.io

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -5,7 +5,7 @@ class ChatEngine{
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
 
-        this.socket = io.connect('http://54.209.103.5:5000/');
+        this.socket = io.connect('http://localhost:5000');
 
         if (this.userEmail){
             this.connectionHandler();
